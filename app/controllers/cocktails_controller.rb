550ed1d3e8cef57fcaa1ec8d602dd6f7946e6351
camille villard <hello@camillevillard.com>
2018-11-08 12:18:30 -0500

    added routes

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,26 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Coktail.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
